3b07134f8c6b064dfdf324081be82890361349b3
wing_shiva <shivatirupathi10@gmail.com>
2025-05-24 01:21:24 +1000

    Exercise and Rest

---
+++ Code Chef/Starters 187/ExerciseAndRest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(n*3);
+	}
+}
