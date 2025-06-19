ffc6b61696e6887a2f98c4cb160888c6960fbf83
wing_shiva <shivatirupathi10@gmail.com>
2025-06-20 02:55:03 +1000

    Reverse Linked List

---
+++ LeetCode/LeetCode75/ReverseLinkedList.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prevNode = null;
+        ListNode nextNode = head;
+        ListNode currNode = head;
+
+        while(nextNode!=null) {
+            nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+
+        return prevNode;
+    }
+}
