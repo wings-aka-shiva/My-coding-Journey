f734815b7027781c8fc33456bff7086ecf32f21f
wing_shiva <shivatirupathi10@gmail.com>
2025-06-26 02:07:51 +1000

    Missing Shoes

---
+++ Code Chef/Starters 192/MissingShoes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner scanner = new Scanner(System.in);
+        int l = scanner.nextInt();
+        int r = scanner.nextInt();
+        System.out.println(Math.abs(l-r));
+	}
+}
