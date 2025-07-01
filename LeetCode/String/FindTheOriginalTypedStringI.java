43935e3c49ba06bc898a3ea109da62bbe83cccfa
wing_shiva <shivatirupathi10@gmail.com>
2025-07-01 17:09:52 +1000

    Find the Original Typed String I

---
+++ LeetCode/String/FindTheOriginalTypedStringI.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        if(word.length() == 0) return 0;
+        if(word.length() == 1) return 1;
+        if(word.length() == 2)    
+            if(word.charAt(0)==word.charAt(1)) return 2;
+            else return 1;
+
+        char previousLetter = word.charAt(0);
+        int count=0;
+        for(int i=1; i<word.length(); ++i) {
+            if(previousLetter==word.charAt(i))
+                count++;
+            previousLetter = word.charAt(i);
+        }
+
+        return count+1;
+    }
+}
